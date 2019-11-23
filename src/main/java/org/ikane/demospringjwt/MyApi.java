5ce6cefa512d3d894fa6a2223e1d8655afe2fb8c
ikane <kaneelhadji2610@yahoo.fr>
2019-11-23 08:29:48 +0100

    refactored version

--- src/main/java/org/ikane/demospringjwt/MyApi.java
+++ src/main/java/org/ikane/demospringjwt/MyApi.java
@@ -1,12 +1,18 @@
 package org.ikane.demospringjwt;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 public class MyApi {
     @GetMapping("/hello")
     public String sayHello() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        log.info("connected user: {}", authentication.getName());
         return "hello";
     }
 }
