46487c0223f09a9317ee7316e9ebddca4c190372
ikane <kaneelhadji2610@yahoo.fr>
2019-12-06 07:03:11 +0100

    refactored version

--- src/main/java/org/ikane/demospringjwt/MyApi.java
+++ src/main/java/org/ikane/demospringjwt/MyApi.java
@@ -3,6 +3,7 @@ package org.ikane.demospringjwt;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,8 +12,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class MyApi {
     @GetMapping("/hello")
     public String sayHello() {
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        log.info("connected user: {}", authentication.getName());
+        JwtAuthenticationToken jwtAuthenticationToken = (JwtAuthenticationToken)SecurityContextHolder.getContext().getAuthentication();
+
+        log.info("connected user: {}", jwtAuthenticationToken.getName());
+        log.info("uid-local claim: {}", jwtAuthenticationToken.getToken().getClaimAsString("uid-local"));
         return "hello";
     }
 }
